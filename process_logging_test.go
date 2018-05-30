0a34d1fff180f22b908495f3500641b5c3c75f10
nerdtakula <33794388+nerdtakula@users.noreply.github.com>
2018-05-30 22:14:21 +1200

    Adding test to read stdout of service

--- process_logging_test.go
+++ process_logging_test.go
@@ -1,8 +1,25 @@
 package supervisor
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestReadProcessStdout(t *testing.T)   { t.Skip("Not Implemented") }
+func TestReadProcessStdout(t *testing.T) {
+	// Setup requirements
+	// setupTeardown := setupTestCase(t)
+	// defer setupTeardown(t)
+
+	// Create client, Read STDOUT of process, compare output
+	client := New(TEST_HOST, TEST_PORT, TEST_USER, TEST_PASS)
+	stdout, err := client.ReadProcessStdout("foobar", 0, 11)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected := "stdout text"; stdout != expected {
+		t.Fatalf("expected '%s', got: '%s'", expected, stdout)
+	}
+}
+
 func TestReadProcessStderr(t *testing.T)   { t.Skip("Not Implemented") }
 func TestTailProcessStdout(t *testing.T)   { t.Skip("Not Implemented") }
 func TestTailProcessStderr(t *testing.T)   { t.Skip("Not Implemented") }
